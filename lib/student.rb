aadbba589f2c29dc484e5ff22c2c3df769b0eb03
Lucy okoth <lucy.okoth@student.moringaschool.com>
2022-10-28 21:07:54 +0300

    Completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
 
+require_relative './user'
+class Student < User
+    
+    def initialize()
+        @knowledge = []
+    end
+
+    def learn(added)
+        @knowledge << added
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
